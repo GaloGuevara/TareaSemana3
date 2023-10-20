15c8ee8c40500837a20fbd6d616c18a91b827557
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 16:28:14 -0500

    \Ej9.c Correcciones de diseño y adición de comentarios

--- Ej9.c
+++ Ej9.c
@@ -1,17 +1,19 @@
 /*Mostrar en pantalla todos los números terminados en 6
 comprendidos entre 25 y 205*/
 
-#include <stdio.h>
-int main()
+#include <stdio.h> //Bibliotecas
+int main()         // Programa main
 {
-    int variacion=10;
-    int num=26;
+    // Declaración de variables
+    int variacion = 10;
+    int num = 26;
 
-    while ((num>=25)&&(num<=205))
+    // Iteraciones while para mostrar los numeros saltando 10
+    while ((num >= 25) && (num <= 205)) // Defines los límites del while con una conjunción &&
     {
-        printf("%d\n", num);
-        num=num+variacion;
+        printf("%d\n", num);   // Mostrar el numero
+        num = num + variacion; // Hacer el cambio y sobreescribir
     }
-    
+
     return 0;
 }
