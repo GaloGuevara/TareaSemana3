d97a1c3b0e4a3683fda0e5848be6d214956b2461
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 17:09:15 -0500

    \Ej29.c Prototipo 1 del código con errores al mostrar el digito

--- Ej29.c
+++ Ej29.c
@@ -9,4 +9,8 @@ int main()
     printf("Ingrese un numero entero: ");
     scanf("%d%*c", &num);
     printf("El numero ingresado es: %d\n", num);
+
+    char letra;
+    letra=num;
+    printf("El primero de sus digitos es: %c", letra);
 }
