1f9f560eb891484cecfef6a79241010db917cbd2
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 17:35:12 -0500

    \Ej39.c Redacción del enunciado

---
+++ Ej39.c
@@ -0,0 +1,8 @@
+/*Se define la serie de Fibonacci como la serie que comienza con los dígitos 1 y 0 y va sumando progresivamente
+los dos ultimos elementos de la serie, así:
+
+    0   1   1   2   3   5   8   13  21  34  ...
+
+Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o sobrepasar el número 10000
+*/
+
