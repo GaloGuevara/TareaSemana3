6b7566e3b2ce5c9efcbc372b05f65635516bdb3c
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 16:32:28 -0500

    \Ej19.c Creación estructura ingresar entero y mostrar el numero ingresado

--- Ej19.c
+++ Ej19.c
@@ -1,1 +1,11 @@
 /*Leer un número entero y determinar si es primo*/
+
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Ingrese un numero entero mayor que cero: ");
+    scanf("%d%*c", &num);
+    printf("El numero ingresado es: %d\n", num);
+}
