e5a4e3fbd0471c62d1c52bb80b5cbf1c3e52d1b3
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 16:48:00 -0500

    \Ej19.c Creación prototipo 1 del programa

--- Ej19.c
+++ Ej19.c
@@ -8,4 +8,33 @@ int main()
     printf("Ingrese un numero entero mayor que cero: ");
     scanf("%d%*c", &num);
     printf("El numero ingresado es: %d\n", num);
+
+    int esPrimo=1; //Supones el numero es primo
+
+    if (num<=0)
+    {
+        printf("El numero ingresado no es primo");
+    } 
+    else 
+    {
+        for (int i = 2; i*i <= num ; i++)
+        {
+            if (num%i==0)
+            {
+                esPrimo=0;
+                break;
+            }
+        }
+
+        if (esPrimo==1)
+        {
+            printf("Es primo");
+        } 
+        else if (esPrimo==0)
+        {
+            printf("No es primo");
+        }
+    }
+    
+    return 0;
 }
