e4e06f8a900323f2c6b7fbdce073fb358d2a04d1
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 21:58:44 -0500

    \Ej49.c Correciones de diseño y adición de comentarios

--- Ej49.c
+++ Ej49.c
@@ -12,68 +12,70 @@
 
 */
 
-#include <stdio.h>
+#include <stdio.h>//bibliotecas
 
-int main()
+int main()//funcion main
 {
-    int i,j,k;
+    int i, j, k;//variables enteras que representan cada columna
 
-    for (int i = 1; i < 10; i++)
-    {   
-        if ((i>0)&&(i<=3))
+    for (int i = 1; i < 10; i++) //iteracion que es la base para la estructura del codigo a partir de la primera columna
+    {
+        if ((i > 0) && (i <= 3)) //condicional para la segunda columna
         {
-            j=1;
+            j = 1;
 
-            if (i==1)
+            //condicionales para la tercera columna
+            if (i == 1) 
             {
-                k=1;
+                k = 1;
             }
-            else if (i==2)
+            else if (i == 2)
             {
-                k=2;
+                k = 2;
             }
-            else if (i==3)
+            else if (i == 3)
             {
-                k=3;
+                k = 3;
             }
-            
         }
-        else if ((i>3)&&(i<=6))
+        else if ((i > 3) && (i <= 6)) //condicional para la segunda columna
         {
-            j=2;
+            j = 2;
 
-            if (i==4)
+            //condicionales para la tercera columna
+            if (i == 4)
             {
-                k=1;
+                k = 1;
             }
-            else if (i==5)
+            else if (i == 5)
             {
-                k=2;
+                k = 2;
             }
-            else if (i==6)
+            else if (i == 6)
             {
-                k=3;
+                k = 3;
             }
-        } 
-        else if ((i>6)&&(i<=9))
-        {
-            j=3;
-
-            if (i==7)
-            {
-                k=1;
-            }
-            else if (i==8)
-            {
-                k=2;
-            }
-            else if (i==9)
-            {
-                k=3;
-            }            
         }
-        
-        printf("%d\t%d\t%d\n", i,j,k);
+        else if ((i > 6) && (i <= 9)) //condicional para la segunda columna
+        {
+            j = 3;
+
+            //condicionales para la tercera columna
+            if (i == 7)
+            {
+                k = 1;
+            }
+            else if (i == 8)
+            {
+                k = 2;
+            }
+            else if (i == 9)
+            {
+                k = 3;
+            }
+        }
+
+        printf("%d\t%d\t%d\n", i, j, k); //Mostrar el resultado de cada fila y va cambiando en cada iteracion del primer for
     }
-    
+    return 0;
 }
