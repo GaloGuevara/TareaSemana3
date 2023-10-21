6fb9eb0c8282f48e3316e81073d4f2eb97864ec1
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-20 21:26:36 -0500

    \Ej39.c Corrección de diseño y adición de comentarios

--- Ej39.c
+++ Ej39.c
@@ -3,24 +3,27 @@ los dos ultimos elementos de la serie, así:
 
     0   1   1   2   3   5   8   13  21  34  ...
 
-Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o sobrepasar el número 10000
+Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o sobrepasar el número 10 000
 */
 
-#include <stdio.h>
+#include <stdio.h> //bibliotecas
 
-int main()
+int main() // funcion main
 {
 
-    int cont0=0, cont1=1;
-    int resul;
+    int cont0 = 0, cont1 = 1; // declaracion de variables iniciales que serviran de contadores
+    int resul;                // variable que manejamos como resultado para ir contanto el resultado
 
-    printf("%d\n%d\n", cont0,cont1);
-    while (resul<=10000)
+    printf("%d\n%d\n", cont0, cont1); // mostramos los dos primeros números que empiezan la serie
+
+    // bloque de iteracion while que para cuando resultado sobrepasa el numero 10 000
+    while (resul <= 10000)
     {
-        resul=cont0 + cont1;
-        printf("%d\n", resul);
-        cont0=cont1;
-        cont1=resul;
+        resul = cont0 + cont1; // guardamos el valor de la suma de los dos terminos anteriores en resultado
+        printf("%d\n", resul); // mostramos el valor de resultado
+        cont0 = cont1;         // reescribimos el valor del contador 0
+        cont1 = resul;         // reescribimos el valor del contador 1
     }
+
     return 0;
 }
